6c674ba9fd680fc43dff1578ca349b73fdf0a6a3
Mike Busyrev <busyrev@gmail.com>
2024-05-11 21:39:02 +0300

    0.22 hotfix

--- cfg.js
+++ cfg.js
@@ -1,6 +1,6 @@
 /** @type {import('./src/Cfg.js').Cfg} */
 module.exports = {
-    version: "0.21",
+    version: "0.22",
     ensureMonotonicSteps: 100, // 10000 works, but slow, its just warranty thats all is ok.
     saltScalesDivisionValue: 0.01,
     waterScalesDivisionValue: 1,
